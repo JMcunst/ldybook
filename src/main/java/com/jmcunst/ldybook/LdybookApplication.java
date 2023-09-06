ded465a62ffd5e6d81ba44d5be31201a132293fd
jmcunst <xnslqjtmghaf@gmail.com>
2023-09-06 21:00:00 +0900

    init

---
+++ src/main/java/com/jmcunst/ldybook/LdybookApplication.java
@@ -0,0 +1,13 @@
+package com.jmcunst.ldybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdybookApplication.class, args);
+	}
+
+}
