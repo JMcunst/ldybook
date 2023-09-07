de77822440f191ec03289324959474e665843c3a
jmcunst <xnslqjtmghaf@gmail.com>
2023-09-07 15:08:55 +0900

    feat : 4 View with mustache

--- src/main/java/com/jmcunst/ldybook/service/posts/PostsService.java
+++ src/main/java/com/jmcunst/ldybook/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jmcunst.ldybook.service.posts;
 
 import com.jmcunst.ldybook.domain.posts.Posts;
 import com.jmcunst.ldybook.domain.posts.PostsRepository;
+import com.jmcunst.ldybook.web.dto.PostsListResponseDto;
 import com.jmcunst.ldybook.web.dto.PostsResponseDto;
 import com.jmcunst.ldybook.web.dto.PostsSaveRequestDto;
 import com.jmcunst.ldybook.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,8 +30,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
